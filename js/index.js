f47ddff3ea4704bf30ca6ea15fd75ea09b30cf13
seeun99 <62701996+seeun99@users.noreply.github.com>
2020-10-28 16:50:48 +0900

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,100 @@
+
+$(document).ready(function () {
+
+
+	$(".button").on("click",function() {
+		$(".green").addClass("animation1")
+	})
+
+	$(".button").on("click",function() {
+		$(".pink").addClass("animation2")
+	})
+
+	$(".button").on("click",function() {
+		$(".yellow").addClass("animation3")
+	})
+
+	$(".button").on("click",function(){
+		$(".sound1")[0].play();
+	})
+
+	$(".button").on("click",function(){
+		$(".sound2")[0].play();
+	})
+
+	$(".button").on("click",function(){
+		$(".sound3")[0].play();
+	})
+
+	$(".button").on("click",function(){
+		$(".sound4")[0].play();
+	})
+
+
+})
+
+
+
+ 
+	// for (var i = 0;i < 6 ;i++) {
+	// 	var ImgHtml = "<img src='../img/circle_1-"+ (i + 1) +".png' style='margin-right: 15%;width: 85%;' class='animation1'>"
+	// 	$(".circle1"). append(ImgHtml);
+	// }
+
+	// for (var i = 0;i < 6 ;i++) {
+	// 	var ImgHtml = "<img src='../img/circle_2-"+ (i + 1) +".png' style='margin-right: 15%;width: 85%;' class='animation2'>"
+	// 	$(".circle2"). append(ImgHtml);
+	// }
+
+	// for (var i = 0;i < 6 ;i++) {
+	// 	var ImgHtml = "<img src='../img/circle_3-"+ (i + 1) +".png' style='margin-right: 15%;width: 85%;' class='animation3'>"
+	// 	$(".circle3"). append(ImgHtml);
+	// }
+
+	// for (var i = 0;i < 16 ;i++) {
+	// 	var ImgHtml = "<img src='../img/circle_4-"+ (i + 1) +".png' style='margin-right: 15%;width: 85%;' class='animation1'>"
+	// 	$(".circle4"). append(ImgHtml);
+	// }
+
+	// for (var i = 0;i < 16 ;i++) {
+	// 	var ImgHtml = "<img src='../img/circle_5-"+ (i + 1) +".png' style='margin-right: 15%;width: 85%;' class='animation2'>"
+	// 	$(".circle5"). append(ImgHtml);
+	// }
+
+	// for (var i = 0;i < 16 ;i++) {
+	// 	var ImgHtml = "<img src='../img/circle_6-"+ (i + 1) +".png' style='margin-right: 15%;width: 85%;' class='animation3'>"
+	// 	$(".circle6"). append(ImgHtml);
+	// }
+
+	// for (var i = 0;i < 13 ;i++) {
+	// 	var ImgHtml = "<img src='../img/circle_7-"+ (i + 1) +".png' style='margin-right: 15%;width: 85%;' class='animation1'>"
+	// 	$(".circle7"). append(ImgHtml);
+	// }
+
+	// for (var i = 0;i < 13 ;i++) {
+	// 	var ImgHtml = "<img src='../img/circle_8-"+ (i + 1) +".png' style='margin-right: 15%;width: 85%;' class='animation2'>"
+	// 	$(".circle8"). append(ImgHtml);
+	// }
+
+	// for (var i = 0;i < 13 ;i++) {
+	// 	var ImgHtml = "<img src='../img/circle_9-"+ (i + 1) +".png' style='margin-right: 15%;width: 85%;' class='animation3'>"
+	// 	$(".circle9"). append(ImgHtml);
+	// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
